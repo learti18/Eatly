fa5c236b497171f3d5dae853ae8ee0e29db91b32
leart <learthyseni12@gmail.com>
2025-04-07 18:11:50 +0200

    Configures user authentication and authorization
    Sets up the database context for user authentication and authorization using ASP.NET Identity.
    
    Adds necessary NuGet packages for Identity, Entity Framework Core, and JWT authentication.
    
    Seeds initial roles (Admin, User) during database creation.
    
    Configures user authentication and authorization
    
    Sets up the database context for user authentication and authorization using ASP.NET Identity.
    
    Adds necessary NuGet packages for Identity, Entity Framework Core, and JWT authentication.
    
    Seeds initial roles (Admin, User) during database creation.

---
+++ Eatly.Server/Eatly.Server/Models/RefreshToken.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eatly.Server.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        [Required]
+        public string DeviceId { get; set; } = string.Empty;
+
+        public DateTime ExpiryTime { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+        public bool isExpired => DateTime.UtcNow >= ExpiryTime;
+    }
+}
--- Eatly.Server/Eatly.Server/Program.cs
+++ Eatly.Server/Eatly.Server/Program.cs
@@ -1,15 +1,19 @@
 using System.Text;
+using dotenv.net;
 using Eatly.Server.Data;
 using Eatly.Server.Identity;
+using Eatly.Server.Interfaces;
 using Eatly.Server.Models;
+using Eatly.Server.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
 builder.Services.AddCors(options =>
@@ -26,12 +30,40 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(connectionString);
 });
+
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
@@ -72,6 +104,8 @@ builder.Services.AddAuthorization(options =>
     options.AddPolicy(IdentityData.AdminUserPolicyName, policy =>
            policy.RequireClaim(IdentityData.AdminUserClaimName, "true"));
 });
+
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build();
 
