50717e869b10a155efec0eb5b3d7be438aa4b223
Rrezon12 <132481643+Rrezon12@users.noreply.github.com>
2025-04-10 02:23:33 +0200

    Enhances styling and blog navigation
    Improves the styling of the accordion component and blog cards using theme variables.
    
    Introduces links to individual blog pages.
    
    Refactors color references to use theme variables for consistency.

--- Eatly.Client/src/components/Accordion.jsx
+++ Eatly.Client/src/components/Accordion.jsx
@@ -10,15 +10,15 @@ export default function Accordion() {
     
     <div className=" bg-transparent  w-full pt-25">
         <div className=" py-16 px-6 md:px-20 max-w-7xl mx-auto flex flex-col items-center relative" >
-            <h2 className="text-3xl md:text-4xl font-bold text-center mb-12 text-gray-800"  >
-            Frequently Asked <br /><span className="text-purple-500">Questions</span>
+            <h2 className="text-3xl md:text-4xl font-bold text-center mb-12 text-text-dark"  >
+            Frequently Asked <br /><span className="text-purple">Questions</span>
             </h2>
-            <img className='absolute left-1/2 ml-40 top-9' src='icon.svg' />
+            <img className='absolute left-1/2 ml-40 top-9 ' src='icon.svg' />
       </div>
 
       <div className="collapse collapse-plus  custom-collapse border border-b-gray-300 rounded-none">
-          <input type="radio" name="faq-accordion" className='text-purple-500' />
-          <div className="collapse-title font-medium text-gray-800 text-base">
+          <input type="radio" name="faq-accordion" className='text-pr' />
+          <div className="collapse-title  font-bold text-text-dark text-base">
             How Does It Work ?
           </div>
           <div className="collapse-content text-gray-600 text-sm">
@@ -27,8 +27,8 @@ export default function Accordion() {
         </div>
 
         <div className="collapse collapse-plus  custom-collapse border border-b-gray-300  rounded-none">
-          <input type="radio" name="faq-accordion" className='text-purple-500' />
-          <div className="collapse-title font-medium text-gray-800 text-base">
+          <input type="radio" name="faq-accordion" className='text-pr' />
+          <div className="collapse-title  font-bold text-text-dark text-base">
             How Does It Work ?
           </div>
           <div className="collapse-content text-gray-600 text-sm">
@@ -38,7 +38,7 @@ export default function Accordion() {
 
         <div className="collapse collapse-plus  custom-collapse border border-b-gray-300  rounded-none">
           <input type="radio" name="faq-accordion" />
-          <div className="collapse-title font-medium text-gray-800 text-base">
+          <div className="collapse-title  font-bold  text-text-dark text-base">
             How does your food delivery service work?
           </div>
           <div className="collapse-content text-gray-600 text-sm">
@@ -48,7 +48,7 @@ export default function Accordion() {
 
         <div className="collapse collapse-plus  custom-collapse border border-b-gray-300  rounded-none">
           <input type="radio" name="faq-accordion" />
-          <div className="collapse-title font-medium text-gray-800 text-base">
+          <div className="collapse-title  font-bold text-gray-800 text-base">
             What payment options do you accept?
           </div>
           <div className="collapse-content text-gray-600 text-sm">
@@ -58,7 +58,7 @@ export default function Accordion() {
 
         <div className="collapse collapse-plus  custom-collapse border border-b-gray-300  rounded-none">
           <input type="radio" name="faq-accordion" />
-          <div className="collapse-title font-medium text-gray-800 text-base">
+          <div className="collapse-title  font-bold text-gray-800 text-base">
             Do you offer discounts or promotions?
           </div>
           <div className="collapse-content text-gray-600 text-sm">
