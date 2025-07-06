5bfff582ba29c48c0fa16835ab7dfe5a8f9e93a7
leart <learthyseni12@gmail.com>
2025-07-06 21:47:17 +0200

    Fixing layout issue in build

--- Eatly.Client/src/Services/Api.js
+++ Eatly.Client/src/Services/Api.js
@@ -3,7 +3,7 @@ import { getAccessToken, isRefreshInProgress, onTokenRefreshed, resetTokenState,
 import { getOrGenerateDeviceId } from './../Utils/GenerateDeviceId';
 import { setCurrentEmail } from './../Utils/UserStore';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5015/api/";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const api = axios.create({
     baseURL: API_BASE_URL,
